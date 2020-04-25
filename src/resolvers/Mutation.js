97d79b176043b7875f46a262f3b90fab7409c44d
neword <newordn@gmail.com>
2020-04-25 14:39:57 +0100

    updated user mutation(if the user is already signed up we just return him)

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,8 @@
 
 const user = async (parent,args,context,info)=>{
     console.log('user mutation')
+    const user1 = await context.prisma.user({name:args.name})
+    if(user1) return user1
     const user = await context.prisma.createUser({...args})
     return user
 }
