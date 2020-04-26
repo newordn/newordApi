ecb0eb325a11ccec73aa0c19d9f3f250b4403c24
neword <newordn@gmail.com>
2020-04-26 17:27:36 +0100

    added conversation resolver in User model

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -2,7 +2,11 @@ const messages= async (parent,args,context,info)=>{
     const messages= await context.prisma.user({id:parent.id}).messages()
     return messages
 }
-
+const conversation= async (parent,args,context,info)=>{
+    const conversation= await context.prisma.user({id:parent.id}).conversation()
+    return conversation
+}
 module.exports={
-    messages
+    messages,
+    conversation
 }
